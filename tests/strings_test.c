90337e1e8381dd4db5c8f0493131f43b593f03f0
Paul Waring <paul@xk7.net>
2025-05-08 11:07:23 +0100

    Force C89 compliance and fix outstanding issues
    This library is intended to comply with C89, however it was using a
    couple of C99 features and the C89 standard was not enforced by the
    compiler.
    
    Changes:
    
    1. Use --std=c89 to require C89 compliance
    
    2. Replace // comments with /* */
    
    3. Replace variable length arrays with heap-allocated memory

--- tests/strings_test.c
+++ tests/strings_test.c
@@ -1,64 +1,75 @@
+#include <stdlib.h>
 #include <string.h>
 
 #include "strings.h"
 #include "unity.h"
 
 void setUp(void) {
-    // Set up
+    /* deliberately empty but must be defined */
 }
 
 void tearDown(void) {
-    // Tear down
+    /* deliberately empty but must be defined */
 }
 
 void test_sub_string_empty_string(void) {
     const char *input = "";
     size_t output_length = strlen(input) + 1;
-    char output[output_length];
+    char *output = calloc(output_length, sizeof(char));
     size_t sub_string_length = sub_string(input, 0, 0, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(0, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("", output);
+
+    free(output);
 }
 
 void test_sub_string_start_end_equal(void) {
     const char *input = "abcde";
     size_t output_length = strlen(input) + 1;
-    char output[output_length];
+    char *output = calloc(output_length, sizeof(char));
     size_t sub_string_length = sub_string(input, 2, 2, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(0, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("", output);
+
+    free(output);
 }
 
 void test_sub_string_first_character(void) {
     const char *input = "abcde";
     size_t output_length = strlen(input) + 1;
-    char output[output_length];
+    char *output = calloc(output_length, sizeof(char));
     size_t sub_string_length = sub_string(input, 0, 1, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(1, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("a", output);
+
+    free(output);
 }
 
 void test_sub_string_last_character(void) {
     const char *input = "abcde";
     size_t output_length = strlen(input) + 1;
-    char output[output_length];
+    char *output = calloc(output_length, sizeof(char));
     size_t sub_string_length = sub_string(input, 4, 5, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(1, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("e", output);
+
+    free(output);
 }
 
 void test_sub_string_whole_string(void) {
     const char *input = "abcde";
     size_t output_length = strlen(input) + 1;
-    char output[output_length];
+    char *output = calloc(output_length, sizeof(char));
     size_t sub_string_length = sub_string(input, 0, 5, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(5, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("abcde", output);
+
+    free(output);
 }
 
 void test_char_count(void) {
